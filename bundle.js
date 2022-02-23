50ab2c7e272bffc0b0fdb3f7ae388fe7fb3626a2
trantuyen082001 <trantuyen082001@gmail.com>
2022-02-23 04:48:48 -1200

    first commit

---
+++ bundle.js
@@ -0,0 +1,57 @@
+(function (React, ReactDOM, d3) {
+  'use strict';
+
+  function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+  var React__default = /*#__PURE__*/_interopDefaultLegacy(React);
+  var ReactDOM__default = /*#__PURE__*/_interopDefaultLegacy(ReactDOM);
+
+  var csvUrl =
+      'https://gist.githubusercontent.com/curran/0ac4077c7fc6390f5dd33bf5c06cb5ff/raw/605c54080c7a93a417a3cea93fd52e7550e76500/UN_Population_2019.csv';
+    
+    var width = 960;
+    var height = 500;
+    
+    var App = function () {
+      var ref = React.useState(null);
+      var data = ref[0];
+      var setData = ref[1];
+    
+      React.useEffect(function () {
+        var row = function (d) {
+          d.Population = +d['2020'];
+          return d;
+        };
+        d3.csv(csvUrl, row).then(function (data) {
+          setData(data.slice(0, 10));
+        });
+      }, []);
+    
+      if (!data) {
+        return React__default["default"].createElement( 'pre', null, "Loading..." );
+      }
+    
+      console.log(data[0]);
+    
+      var yScale = d3.scaleBand()
+        .domain(data.map(function (d) { return d.Country; }))
+        .range([0, height]);
+    
+      var xScale = d3.scaleLinear()
+        .domain([0, d3.max(data, function (d) { return d.Population; })])
+        .range([0, width]);
+    
+      return (
+        React__default["default"].createElement( 'svg', { width: width, height: height }, 
+          data.map(function (d) { return (
+            React__default["default"].createElement( 'rect', {
+              key: d.Country, y: yScale(d.Country), width: xScale(d.Population), height: yScale.bandwidth() })
+          ); })
+        )
+      );
+    };
+    var rootElement = document.getElementById('root');
+    ReactDOM__default["default"].render(React__default["default"].createElement( App, null ), rootElement);
+
+})(React, ReactDOM, d3);
+//# sourceMappingURL=bundle.js.map
---
+++ index.js
@@ -0,0 +1,58 @@
+import React, {
+    useState,
+    useCallback,
+    useEffect,
+  } from 'react';
+  import ReactDOM from 'react-dom';
+  import { csv, scaleBand, scaleLinear, max } from 'd3';
+  
+  const csvUrl =
+    'https://gist.githubusercontent.com/curran/0ac4077c7fc6390f5dd33bf5c06cb5ff/raw/605c54080c7a93a417a3cea93fd52e7550e76500/UN_Population_2019.csv';
+  
+  const width = 960;
+  const height = 500;
+  const margin = {top: 20, right: 20, bottom: 20, left: 20};
+  
+  const App = () => {
+    const [data, setData] = useState(null);
+  
+    useEffect(() => {
+      const row = (d) => {
+        d.Population = +d['2020'];
+        return d;
+      };
+      csv(csvUrl, row).then((data) => {
+        setData(data.slice(0, 10));
+      });
+    }, []);
+  
+    if (!data) {
+      return <pre>Loading...</pre>;
+    }
+  
+    console.log(data[0]);
+  
+    const yScale = scaleBand()
+      .domain(data.map((d) => d.Country))
+      .range([0, height]);
+  
+    const xScale = scaleLinear()
+      .domain([0, max(data, (d) => d.Population)])
+      .range([0, width]);
+  
+    return (
+      <svg width={width} height={height}>
+        {data.map((d) => (
+          <rect
+            key={d.Country}
+            y={yScale(d.Country)}
+            width={xScale(d.Population)}
+            height={yScale.bandwidth()}
+          />
+        ))}
+      </svg>
+    );
+  };
+  const rootElement = document.getElementById('root');
+  ReactDOM.render(<App />, rootElement);
+  
